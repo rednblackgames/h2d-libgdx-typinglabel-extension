185801e5451310429637d6695a2c6b51e92841dd
fgnm <fgnm.dev@gmail.com>
2021-12-19 01:33:41 +0100

    Merge VO serializer into new factories

--- src/main/java/games/rednblack/h2d/extension/typinglabel/TypingLabelComponentFactory.java
+++ src/main/java/games/rednblack/h2d/extension/typinglabel/TypingLabelComponentFactory.java
@@ -3,26 +3,26 @@ package games.rednblack.h2d.extension.typinglabel;
 import com.artemis.ComponentMapper;
 import com.artemis.EntityTransmuter;
 import com.artemis.EntityTransmuterFactory;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.physics.box2d.World;
-import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
+import com.badlogic.gdx.utils.Pool;
+import com.badlogic.gdx.utils.Pools;
 import games.rednblack.editor.renderer.box2dLight.RayHandler;
 import games.rednblack.editor.renderer.components.DimensionsComponent;
-import games.rednblack.editor.renderer.components.ParentNodeComponent;
 import games.rednblack.editor.renderer.components.label.LabelComponent;
 import games.rednblack.editor.renderer.data.LabelVO;
 import games.rednblack.editor.renderer.data.MainItemVO;
 import games.rednblack.editor.renderer.data.ProjectInfoVO;
 import games.rednblack.editor.renderer.data.ResolutionEntryVO;
-import games.rednblack.editor.renderer.factory.EntityFactory;
 import games.rednblack.editor.renderer.factory.component.ComponentFactory;
+import games.rednblack.editor.renderer.factory.EntityFactory;
+import games.rednblack.editor.renderer.factory.component.LabelComponentFactory;
 import games.rednblack.editor.renderer.resources.IResourceRetriever;
 
 public class TypingLabelComponentFactory extends ComponentFactory {
 
     protected ComponentMapper<LabelComponent> labelCM;
     protected ComponentMapper<TypingLabelComponent> typingLabelCM;
-
-    private static int labelDefaultSize = 12;
 
     private EntityTransmuter transmuter;
 
@@ -35,58 +35,93 @@ public class TypingLabelComponentFactory extends ComponentFactory {
         super.injectDependencies(engine, rayHandler, world, rm);
 
         transmuter = new EntityTransmuterFactory(engine)
-                .add(ParentNodeComponent.class)
                 .add(LabelComponent.class)
                 .add(TypingLabelComponent.class)
                 .build();
     }
 
     @Override
-    public int createSpecialisedEntity(int root, MainItemVO vo) {
-        int entity = createGeneralEntity(vo, EntityFactory.LABEL_TYPE);
+    public void transmuteEntity(int entity) {
         transmuter.transmute(entity);
-
-        adjustNodeHierarchy(root, entity);
-
-        initializeLabelComponent(labelCM.get(entity), (LabelVO) vo);
-        checkTypingLabelComponent(entity, (LabelVO) vo);
-
-        return entity;
     }
 
-    protected void initializeDimensionsComponent(int entity, DimensionsComponent component, MainItemVO vo) {
-        component.height = ((LabelVO) vo).height;
-        component.width = ((LabelVO) vo).width;
+    @Override
+    public int getEntityType() {
+        return EntityFactory.LABEL_TYPE;
     }
 
-    protected void initializeLabelComponent(LabelComponent component, LabelVO vo) {
-        component.setText(vo.text);
-        component.setStyle(generateStyle(rm, vo.style, vo.size, vo.monoSpace));
-        component.fontName = vo.style;
-        component.fontSize = vo.size;
-        component.mono = vo.monoSpace;
-        component.setAlignment(vo.align);
-        component.setWrap(vo.wrap);
-        component.typingEffect = vo.isTyping;
+    @Override
+    public void setInitialData(int entity, Object data) {
+        LabelComponent component = labelCM.get(entity);
+        Object[] params = (Object[]) data;
+        component.setText((String) params[0]);
+        component.fontName = (String) params[1];
+        component.fontSize = (int) params[2];
+        component.mono = (boolean) params[3];
+        component.typingEffect = (boolean) params[4];
+    }
+
+    @Override
+    public Class<LabelVO> getVOType() {
+        return LabelVO.class;
+    }
+
+    @Override
+    public void initializeSpecialComponentsFromVO(int entity, MainItemVO voG) {
+        LabelVO vo = (LabelVO) voG;
+        DimensionsComponent dimensionsComponent = dimensionsCM.get(entity);
+        dimensionsComponent.height = vo.height;
+        dimensionsComponent.width = vo.width;
+
+        LabelComponent labelComponent = labelCM.get(entity);
+        labelComponent.setText(vo.text);
+        labelComponent.setStyle(LabelComponentFactory.generateStyle(rm, vo.style, vo.size, vo.monoSpace));
+        labelComponent.fontName = vo.style;
+        labelComponent.fontSize = vo.size;
+        labelComponent.mono = vo.monoSpace;
+        labelComponent.setAlignment(vo.align);
+        labelComponent.setWrap(vo.wrap);
+        labelComponent.typingEffect = vo.isTyping;
+
+        ProjectInfoVO projectInfoVO = rm.getProjectVO();
+        ResolutionEntryVO resolutionEntryVO = rm.getLoadedResolution();
+        float multiplier = resolutionEntryVO.getMultiplier(rm.getProjectVO().originalResolution);
+
+        labelComponent.setFontScale(multiplier / projectInfoVO.pixelToWorld);
+    }
+
+    @Override
+    protected void initializeTransientComponents(int entity) {
+        super.initializeTransientComponents(entity);
+
+        LabelComponent component = labelCM.get(entity);
+        component.setStyle(LabelComponentFactory.generateStyle(rm, component.fontName, component.fontSize, component.mono));
 
         ProjectInfoVO projectInfoVO = rm.getProjectVO();
         ResolutionEntryVO resolutionEntryVO = rm.getLoadedResolution();
         float multiplier = resolutionEntryVO.getMultiplier(rm.getProjectVO().originalResolution);
 
         component.setFontScale(multiplier / projectInfoVO.pixelToWorld);
-    }
 
-    protected void checkTypingLabelComponent(int entity, LabelVO vo) {
-        if (!vo.isTyping) typingLabelCM.remove(entity);
-    }
-
-
-    public static LabelStyle generateStyle(IResourceRetriever rManager, String fontName, int size, boolean mono) {
-
-        if (size == 0) {
-            size = labelDefaultSize;
+        if (!component.typingEffect) {
+            typingLabelCM.remove(entity);
         }
-        return new LabelStyle(rManager.getBitmapFont(fontName, size, mono), null);
     }
 
+    @Override
+    protected void initializeDimensionsComponent(int entity) {
+        ProjectInfoVO projectInfoVO = rm.getProjectVO();
+        LabelComponent labelComponent = labelCM.get(entity);
+        DimensionsComponent component = dimensionsCM.get(entity);
+        if (component.width == 0 && component.height == 0) {
+            Pool<GlyphLayout> layoutPool = Pools.get(GlyphLayout.class);
+            GlyphLayout layout = layoutPool.obtain();
+            layout.setText(labelComponent.cache.getFont(), labelComponent.getText());
+            component.width = layout.width / projectInfoVO.pixelToWorld;
+            component.width += (component.width * 20) / 100;
+            component.height = layout.height / projectInfoVO.pixelToWorld;
+            component.height += (component.height * 40) / 100;
+            layoutPool.free(layout);
+        }
+    }
 }
--- src/main/java/games/rednblack/h2d/extension/typinglabel/TypingLabelItemType.java
+++ src/main/java/games/rednblack/h2d/extension/typinglabel/TypingLabelItemType.java
@@ -2,22 +2,20 @@ package games.rednblack.h2d.extension.typinglabel;
 
 import com.artemis.systems.IteratingSystem;
 import games.rednblack.editor.renderer.commons.IExternalItemType;
+import games.rednblack.editor.renderer.data.LabelVO;
 import games.rednblack.editor.renderer.factory.EntityFactory;
 import games.rednblack.editor.renderer.factory.component.ComponentFactory;
-import games.rednblack.editor.renderer.factory.v2.ComponentFactoryV2;
 import games.rednblack.editor.renderer.systems.render.logic.DrawableLogic;
 import games.rednblack.editor.renderer.utils.ComponentRetriever;
 
 public class TypingLabelItemType implements IExternalItemType {
 
     private ComponentFactory factory;
-    private ComponentFactoryV2 factoryV2;
     private IteratingSystem system;
     private DrawableLogic drawableLogic;
 
     public TypingLabelItemType() {
         factory = new TypingLabelComponentFactory();
-        factoryV2 = new TypingLabelComponentFactoryV2();
         system = new TypingLabelSystem();
         drawableLogic = new TypingLabelDrawableLogic();
     }
@@ -40,11 +38,6 @@ public class TypingLabelItemType implements IExternalItemType {
     @Override
     public ComponentFactory getComponentFactory() {
         return factory;
-    }
-
-    @Override
-    public ComponentFactoryV2 getComponentFactoryV2() {
-        return factoryV2;
     }
 
     @Override
