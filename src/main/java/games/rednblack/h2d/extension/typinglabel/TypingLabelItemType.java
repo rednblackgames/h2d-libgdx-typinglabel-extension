c2690a20e90a3c1627601c532a80a8a7e139e3cc
fgnm <fgnm.dev@gmail.com>
2021-12-27 21:29:06 +0100

    Extensions can really load data in `ResourcesManager`

--- src/main/java/games/rednblack/h2d/extension/typinglabel/TypingLabelItemType.java
+++ src/main/java/games/rednblack/h2d/extension/typinglabel/TypingLabelItemType.java
@@ -1,12 +1,15 @@
 package games.rednblack.h2d.extension.typinglabel;
 
 import com.artemis.systems.IteratingSystem;
+import com.badlogic.gdx.utils.ObjectSet;
 import games.rednblack.editor.renderer.commons.IExternalItemType;
-import games.rednblack.editor.renderer.data.LabelVO;
 import games.rednblack.editor.renderer.factory.EntityFactory;
 import games.rednblack.editor.renderer.factory.component.ComponentFactory;
+import games.rednblack.editor.renderer.resources.IResourceRetriever;
 import games.rednblack.editor.renderer.systems.render.logic.DrawableLogic;
 import games.rednblack.editor.renderer.utils.ComponentRetriever;
+
+import java.util.HashMap;
 
 public class TypingLabelItemType implements IExternalItemType {
 
@@ -51,7 +54,12 @@ public class TypingLabelItemType implements IExternalItemType {
     }
 
     @Override
-    public String formatResourcePath(String resName) {
+    public void loadExternalTypesAsync(IResourceRetriever rm, ObjectSet<String> assetsToLoad, HashMap<String, Object> assets) {
+        throw new RuntimeException("Method not implemented.");
+    }
+
+    @Override
+    public void loadExternalTypesSync(IResourceRetriever rm, ObjectSet<String> assetsToLoad, HashMap<String, Object> assets) {
         throw new RuntimeException("Method not implemented.");
     }
 }
